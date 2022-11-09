e0fc401f5bcd5e70acac6d2d7b0df992d2fa2aa2
asif3062 <asifbsb2002@gmail.com>
2022-11-09 16:53:18 +0600

    router set up added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Pages/Main/Main';
+import Home from './Pages/Common/Home';
+import Services from './Pages/Common/Services';
 
 function App() {
+
+  const routes = createBrowserRouter([
+    {
+      path : '/',
+      element : <Main></Main>,
+      children : [
+        {
+          path : '/',
+          element : <Home></Home>
+        },
+        {
+          path : '/services',
+          element : <Services></Services>
+        }
+      ]
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
