a4779fb5a073c8f714c5ecf24ed09f431b10831c
asif3062 <asifbsb2002@gmail.com>
2022-11-09 18:37:40 +0600

    faq section added

--- src/Pages/Common/Home.js
+++ src/Pages/Common/Home.js
@@ -17,6 +17,49 @@ const Home = () => {
             <div className='text-center my-3'>
                 <Link to='/services'><button className='py-2 px-5 rounded-md bg-cyan-400 text-white'>See All</button></Link>
             </div>
+
+                {/* faq section  */}
+
+            <h2 className='text-xl my-3'>Frequently Asked Questions</h2>
+            <div className='px-10 md:px-28 mx-auto'>
+                <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-2">
+                    <div className="collapse-title text-xl font-medium">
+                        What are the common dental problems?
+                    </div>
+                    <div className="collapse-content">
+                        <p>Tooth Decay(Tooth decay is also known as dental caries or dental cavities),
+                            Gum Disease(Gingivitis is the early stage and mild form of gum or periodontal disease),
+                            Bad Breath,
+                            Sensitive Teeth,
+                            Cracked or Broken Teeth,
+                            Receding Gums,
+                            Root Infection,
+                            Enamel Erosion.</p>
+                    </div>
+                </div>
+                <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-2">
+                    <div className="collapse-title text-xl font-medium">
+                        How to take care of our teeth?
+                    </div>
+                    <div className="collapse-content">
+                        Floss at least once per day,
+                        Brush your teeth twice a day with a soft-bristled toothbrush,
+                        Use fluoride toothpaste,
+                        Replace your toothbrush every 3 to 4 months or sooner if needed,
+                        Eat a healthy diet,
+                        Avoid sweets and sweetened drinks,
+                        Do not smoke.
+                    </div>
+                </div>
+                <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-2">
+                    <div className="collapse-title text-xl font-medium">
+                        Do we need to brush our teeth regularly?
+                    </div>
+                    <div className="collapse-content">
+                        The American Dental Association recommends brushing your teeth twice a day with fluoride toothpaste for two minutes each time. When you brush your teeth, you help remove food and plaque — a sticky white film that forms on your teeth and contains bacteria.
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Common/ServiceDetails.js
+++ src/Pages/Common/ServiceDetails.js
@@ -7,9 +7,9 @@ const ServiceDetails = () => {
     const service = useLoaderData()
     const { _id, name, img, desc, price } = service
     return (
-        <div className='my-4 flex'>
+        <div className='my-4 flex flex-col md:flex-row'>
             {/* service details  */}
-            <div className='flex-0 p-2 md:p-4 w-full lg:w-56 border-r-2'>
+            <div className='md:flex-0 p-2 md:p-4 w-full lg:w-72 border-r-2'>
                 <h1 className='text-xl font-semibold underline mb-2'>Service Details</h1>
                 <img src={img} className='w-full' alt="" />
                 <div>
@@ -19,7 +19,7 @@ const ServiceDetails = () => {
                 </div>
             </div>
             {/* reviews */}
-            <div className='flex-grow p-2 md:p-4'>
+            <div className='md:flex-grow p-2 md:p-4'>
                 <h1 className='text-xl font-semibold underline mb-2'>Reviews</h1>
             </div>
         </div>
