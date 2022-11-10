fd54d42286086f65a8ce995d5430234cbfaa81f0
asif3062 <asifbsb2002@gmail.com>
2022-11-10 15:18:50 +0600

    review delete added

--- src/Pages/MyReviews/MyRevies.js
+++ src/Pages/MyReviews/MyRevies.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Context/ContextProvider';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MyRevies = () => {
 
@@ -13,8 +15,39 @@ const MyRevies = () => {
             })
     }, [user?.email])
 
+    const handleDeleteReview=(id)=>{
+        console.log('id from btn handler',id)
+
+        fetch(`http://localhost:5000/reviews/${id}`,{
+            method : "DELETE"
+        })
+        .then(res=>res.json())
+        .then(()=>{
+            toast.error('Review Deleted')
+        })
+        const remainingReviews = myreviews.filter(rev => rev._id !== id)
+        setMyreviews(remainingReviews)
+
+    }
+
+    const handleEditReview=(id)=>{
+
+    }
+
     return (
         <div className='w-full p-5 md:w-3/4 mx-auto h-screen'>
+            <ToastContainer
+                position="top-center"
+                autoClose={2500}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
             {
                 myreviews.length > 0 ? <>
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
@@ -28,8 +61,8 @@ const MyRevies = () => {
                                     </div>
                                     <p><span className='font-bold italic'>Review</span> : {review.reviewText}</p>
                                     <div className='flex flex-start items-center mt-5'>
-                                        <button className='bg-rose-600 py-2 px-4 text-white rounded-md mx-3'>Delete</button>
-                                        <button className='bg-blue-600 py-2 px-4 text-white rounded-md mx-3'>Edit</button>
+                                        <button onClick={()=>handleDeleteReview(review._id)} className='bg-rose-600 py-2 px-4 text-white rounded-md mx-3'>Delete</button>
+                                        <button onClick={()=>handleEditReview(review._id)} className='bg-blue-600 py-2 px-4 text-white rounded-md mx-3'>Edit</button>
                                     </div>
                                 </div>
                             </div>)
