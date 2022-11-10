261a5db78d9cd1c13f6c3e06497edf481c3cddb1
asif3062 <asifbsb2002@gmail.com>
2022-11-10 20:41:45 +0600

    little changes

--- src/Pages/Shared/ServiceCard.js
+++ src/Pages/Shared/ServiceCard.js
@@ -15,7 +15,7 @@ const ServiceCard = ({ service }) => {
             <div>
                 <h1 className='font-semibold'>{name}</h1>
                 <h1 className='font-semibold'>Price : ${price}</h1>
-                <p>{desc.length > 100 ? desc.slice(0,100) : desc}</p>
+                <p>{desc.length > 100 ? desc.slice(0,100)+'...' : desc}</p>
             </div>
             <Link to={`/services/${_id}`} className='py-2 px-5 mt-2 text-white bg-cyan-500 rounded-md'><button>View Details</button></Link>
         </div>
